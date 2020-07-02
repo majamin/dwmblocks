28d132a2adeb6a5aa2298a50977520d4ccc0c793
Marian Minar <majamin@gmail.com>
2020-07-01 23:04:13 -0700

    adding all of the statusbar scripts

--- blocks.h
+++ blocks.h
@@ -1,20 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	/*{"", "~/bin/statusbar/volume",					0,		10},*/
+	{"",	"pacpackages",	0,	8},
+	{"", "~/.local/bin/statusbar/barvol",					1,		10},
+	{"", "~/.local/bin/statusbar/bardate",					60,		1},
+	{"", "~/.local/bin/statusbar/bartime",					60,		1},
+	{"", "~/.local/bin/statusbar/barbatt",					5,		0},
+	{"",	"internet",	5,	4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';

6c330fd221f2aa425edb4835a53705f3bba008f1
Marian Minar <majamin@gmail.com>
2020-07-24 11:14:13 -0700

    clean-up

--- blocks.h
+++ blocks.h
@@ -1,13 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/*{"", "~/bin/statusbar/volume",					0,		10},*/
-	{"", "~/.local/bin/statusbar/barpac",					3600,		8},
-	{"", "~/.local/bin/statusbar/barvol",					1,		10},
-	{"", "~/.local/bin/statusbar/bardate",					60,		1},
-	{"", "~/.local/bin/statusbar/bartime",					60,		1},
-	{"", "~/.local/bin/statusbar/barbatt",					5,		0},
-	{"",	"internet",							5,		4},
+	{"", "barpac",					3600,		8},
+	{"", "barvol",					1,		10},
+	{"", "bardate",					60,		1},
+	{"", "bartime",					60,		1},
+	{"", "barbatt",					5,		0},
+	{"", "barnet",					5,		4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

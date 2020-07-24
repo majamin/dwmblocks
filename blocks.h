8f02870c8cfea24912a33789cb3ff903a552f0c4
Marian Minar <majamin@gmail.com>
2020-07-24 09:49:55 -0700

    package updates were not being resfreshed ... oops

--- blocks.h
+++ blocks.h
@@ -2,12 +2,12 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/*{"", "~/bin/statusbar/volume",					0,		10},*/
-	{"", "~/.local/bin/statusbar/barpac",					0,	8},
+	{"", "~/.local/bin/statusbar/barpac",					3600,		8},
 	{"", "~/.local/bin/statusbar/barvol",					1,		10},
 	{"", "~/.local/bin/statusbar/bardate",					60,		1},
 	{"", "~/.local/bin/statusbar/bartime",					60,		1},
 	{"", "~/.local/bin/statusbar/barbatt",					5,		0},
-	{"",	"internet",							5,	4},
+	{"",	"internet",							5,		4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

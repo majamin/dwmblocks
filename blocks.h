61625dd2420f8af1ee9505b64675093c384e1bd5
Marian Minar <majamin@gmail.com>
2020-10-16 13:57:36 -0700

    adding statusbar inbox count

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "barmail",					180,		12},
 	{"", "barpac",					3600,		8},
 	{"", "barvol",					1,		10},
 	{"", "bardate",					60,		1},

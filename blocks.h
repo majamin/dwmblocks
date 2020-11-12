5e206d31aecabd41b0852eb22fd2007f45546081
Marian Minar <majamin@gmail.com>
2020-11-11 21:56:27 -0800

    changed some timings

--- blocks.h
+++ blocks.h
@@ -2,12 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "barmail",					180,		12},
-	{"", "barpac",					3600,		8},
+	{"", "barpac",					600,		8},
 	{"", "barvol",					1,		10},
-	{"", "bardate",					60,		1},
-	{"", "bartime",					60,		1},
-	{"", "barbatt",					5,		0},
-	{"", "barssid",					5,		4},
+	{"", "bardate",					10,		1},
+	{"", "bartime",					10,		1},
+	{"", "barbatt",					2,		0},
+	{"", "barssid",					2,		4},
+	{"", "barblue",					1,		10},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

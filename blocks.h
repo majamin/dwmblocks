03dd0d5079c26d49d2b43d9f602e0565cae0bd4a
Marian Minar <majamin@gmail.com>
2020-11-16 10:33:09 -0800

    barpac and barvol update times

--- blocks.h
+++ blocks.h
@@ -2,8 +2,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "barmail",					180,		12},
-	{"", "barpac",					600,		8},
-	{"", "barvol",					1,		10},
+	{"", "barpac",					0,		8},
+	{"", "barvol",					0,		10},
 	{"", "bardate",					10,		1},
 	{"", "bartime",					10,		1},
 	{"", "barbatt",					2,		0},

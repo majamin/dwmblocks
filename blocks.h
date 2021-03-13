7944dd3a4f0947f7a514d439d930efcab649c2b3
Marian Minar <majamin@gmail.com>
2021-03-13 10:09:46 -0800

    faster barclipyt update

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "barclipyt",				2,		0},
+	{"", "barclipyt",				1,		0},
 	{"", "bargit",					10,		1},
 	{"", "barvol",					0,		10},
 	{"", "bartime",					10,		1},
